b0c820f5aa74420c1a72dbf4597c16e39698c09f
Will <WittenauerW@cbchs.org>
2024-09-20 09:28:53 -0500

    made loops

--- going_loopy_wittenauer.py
+++ going_loopy_wittenauer.py
@@ -3,14 +3,15 @@ for i in range(1, 16, 2):
     print(i)
 
 
-
+x = 1
 ingredients = ["lettuce", "tomato", "turkey", "peppers", "cheese"]
 for ingredient in ingredients:
-    print(f"Adding {ingredient}.")
+    print(f"{x}. Adding {ingredient}.")
+    x = x + 1
 print("Your sandwich is complete.")
     
 
-    
+
 earth_weight = 120
 for year in range(1, 16):
     earth_weight = earth_weight + 1
