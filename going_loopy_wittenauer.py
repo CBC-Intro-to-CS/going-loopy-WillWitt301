a91dc771259bb873511e5c4ccb9365d119e0bdcd
Will <WittenauerW@cbchs.org>
2024-09-19 21:29:02 -0500

    loopy

---
+++ going_loopy_wittenauer.py
@@ -0,0 +1,30 @@
+n = 1
+age = 15
+while n >= 1:
+    print(n)
+    n = n + 2
+    if n > age:
+        break
+
+ingredients = ["lettuce", "tomato", "turkey", "peppers", "cheese"]
+for ingredient in ingredients:
+    print(f"Adding {ingredient}.")
+print("Your sandwich is complete.")
+    
+earth_weight = 120 
+moon_weight = earth_weight * 0.165
+
+years = [1,2,3,4,5,6,7,8,9,10,12,12,13,14,15]
+earth_weight = 120
+while earth_weight >= 120:
+    earth_weight = earth_weight + 1
+    if earth_weight > 135:
+        break
+    for year in years:
+        moon_weight = earth_weight * 0.165
+        print("Year %s = %s" % (year, moon_weight))
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,25 @@
+# n = 5 
+# while n > 0:
+#     print(n) 
+#     n = n - 1
+# print("Blastoff!")
+
+# toppings = ["mushrooms", "pepperoni", "sausage", "extra cheese"]
+# for topping in toppings:
+#     print(f"Adding {topping}")
+# print("Finished making your pizza!")
+
+# for x in range(0, 5):
+#     print(f"Hello {x}")
+# #   print("Hello %s" % x)
+
+# found_coins = 20
+# magic_coins = 70
+# stolen_coins = 3
+# coins = found_coins
+# for week in range (1, 53):
+#     coins = coins + magic_coins - stolen_coins
+#     print("Week %s = %s" % (week, coins))
+
+earth_weight = 120
+moon_weight
