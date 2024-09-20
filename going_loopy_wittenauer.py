6b2ebb3254f9fd318df2ae72ecf35153a5e73cb4
Will <WittenauerW@cbchs.org>
2024-09-20 09:13:26 -0500

    made loops for homework

--- going_loopy_wittenauer.py
+++ going_loopy_wittenauer.py
@@ -1,27 +1,22 @@
-n = 1
-age = 15
-while n >= 1:
-    print(n)
-    n = n + 2
-    if n > age:
-        break
+age = 15 
+for i in range(1, 16, 2):
+    print(i)
+
+
 
 ingredients = ["lettuce", "tomato", "turkey", "peppers", "cheese"]
 for ingredient in ingredients:
     print(f"Adding {ingredient}.")
 print("Your sandwich is complete.")
     
-earth_weight = 120 
 
-years = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    
 earth_weight = 120
-while earth_weight >= 120:
+for year in range(1, 16):
     earth_weight = earth_weight + 1
-    if earth_weight > 135:
-        break
-    for year in years:
-        moon_weight = earth_weight * 0.165
-    print(moon_weight)
+    moon_weight = earth_weight * 0.165
+    print(f"Year {year} is {moon_weight}")
+
 
 
 
